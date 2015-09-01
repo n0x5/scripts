395b73d5178662d2273a02992addcdc10dc53c89
n0x5 <coax@audiolabs.org>
2015-09-01 19:26:22 +0200

    Create listfiles.py

---
+++ listfiles.py
@@ -0,0 +1,24 @@
+# python 3
+# List files, set options at the top
+
+# Name of file to write to, relative or absolute
+fname = 'output.txt'
+# Path to folder to scan
+drpath = '/path/to/folder'
+
+##################################################
+
+from os import walk
+
+str(drpath)
+f = []
+for (dirpath, dirnames, filenames) in walk(drpath):
+    #f.extend('{0!s}'.format(ftype))
+    #for f in dirnames:
+        
+    f.extend(dirnames)
+    b = open( fname, 'a' )
+    b.write("\n".join(f))
+    b.close()
+    
+    break
