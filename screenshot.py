ec9160ea7b82ac03bac000178d674c37ed94c28f
n0x5 <coax@audiolabs.org>
2015-09-01 19:29:25 +0200

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 import time
+import sys
 
 driver = webdriver.PhantomJS()
 htt = 'http://'
-url2 = 'site.com'
+url2 = sys.argv[1]
 url = htt + url2
 today = time.strftime("_%m_%Y")
 
