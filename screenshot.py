15f03daac9c5d28f6cc81bbc4e117465de843721
n0x5 <coax@audiolabs.org>
2015-09-01 23:57:55 +0200

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -6,7 +6,7 @@ driver = webdriver.PhantomJS()
 htt = 'http://'
 url2 = sys.argv[1]
 url = htt + url2
-today = time.strftime("__%m_%Y_%R")
+today = time.strftime("__%m_%Y_%H_%M")
 
 driver.get(url)
 driver.get_screenshot_as_file('screen_{1}_{0}.jpg' .format(today, url2))
