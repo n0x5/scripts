4e2cf40ca5100a89b9fbdff5e6b1a7728a488f42
n0x5 <coax@audiolabs.org>
2015-09-06 12:39:21 +0200

    Update sfv.py

--- sfv.py
+++ sfv.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
-# Python SFV Creator sfv.py <file> 
 import sys
-import binascii
+import zlib
 
 filename = sys.argv[1]
 
 def crc32(filename):
     sf = open(filename,'rb').read()
-    sf = hex(binascii.crc32(sf) % (1<<32)).replace('0x', '')
+    sf = format(zlib.crc32(sf), '08x')
     print ('{}' .format(sf))
 
 crc32(filename)
