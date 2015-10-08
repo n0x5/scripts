e3b4c984a2eccdb0d578517cb38a419f41e86d13
n0x5 <coax@audiolabs.org>
2015-10-08 18:03:02 +0200

    Create mp3genresort.py
    automatically creates genre hard links in windows for a list of mp3s

---
+++ mp3genresort.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import shutil
+import os
+import errno
+from mutagen.id3 import ID3
+
+rootdir = 'F:\python\mp3'
+
+
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        if file.endswith(".mp3"):
+            try:
+                file2 = os.path.join(subdir, file)
+                audio = ID3(file2)
+                sf = format(audio['TCON'].text[0])
+                path2 = os.path.join(subdir)
+                rootdir2 = 'F:\python\mp3\{}' .format(sf)
+                if not os.path.exists(rootdir2): os.makedirs(rootdir2)
+                basenm2 = os.path.basename(path2)
+                basen3 = path2, os.path.join(rootdir2, basenm2)
+                shutil.copytree(path2, os.path.join(rootdir2, basenm2), copy_function=os.link)
+            except:
+                pass
