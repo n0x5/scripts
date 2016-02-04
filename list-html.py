e4b1a8ee46edb8012b471727f4843ea2bc6a945d
n0x5 <coax@audiolabs.org>
2016-02-04 16:42:59 +0100

    Update list-html.py

--- list-html.py
+++ list-html.py
@@ -8,18 +8,16 @@ today = time.strftime("__%m_%Y_%H_%M_%S")
 
 cwd = os.getcwd()
 os.chdir(cwd)
-
+number = 1
 rootdir = cwd
-
 fname = 'list_{}_{}.html' .format(os.path.basename(cwd), today)
 b = open( fname, 'a' )
 b.write('<!DOCTYPE html><html><body><h2><a href="{}">{} List</a></h2><table class="sortable" style="width:100%"><script src="sorttable.js"></script>' .format(fname, os.path.basename(cwd)))
 b.write('<tr><th style="text-align:left">Release</th><th style="text-align:left">Group</th><th style="text-align:left">Genre</th><th style="text-align:left">Format</th></tr>\n')
 
 def store(title, grp, genre):
-    print ("(\"{}\" \"{}\" \"{}\" \"{}\")" .format(basenm2, file6, genrs(file2), file7))
+    print ("(\"{}\" \"{}\" \"{}\" \"{}\" \"{}\")" .format(basenm2, file6, genrs(file2), file7, number))
     b.write("<tr><td>{}</td>  <td>{}</td> <td>{}</td><td>{}</td></tr>\n" .format(basenm2, file6, genrs(file2), file7))
-    
 
 def genrs(fn):
     filn = open(fn, "r")
@@ -65,8 +63,9 @@ for subdir, dirs, files in os.walk(rootdir):
                 file6 = "[]".join(basenm2.split('-')[-1:])
                 file7 = "[]".join(basenm2.split('.')[-1:]).split('-')[0]
                 store(basenm2, file6, genrs(file2))
+                number += 1
             except:
                 pass
-
+b.write("<div class=\"total\" style=\"font-weight:bold;\">Total number of items: {} </br></br></div>" .format(number))
 b.write("</table>\n</body>\n</html>")
 b.close()
