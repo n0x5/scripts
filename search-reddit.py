439518cee7096a0459208535f04bb462724dfa81
n0x5 <coax@audiolabs.org>
2016-05-24 00:38:47 +0200

    Create search-reddit.py

---
+++ search-reddit.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import time
+import sys
+import argparse
+from time import mktime
+from datetime import datetime
+import calendar
+
+def crdate(datestr):
+    return time.strptime(datestr, '%Y-%m-%d')
+
+parser=argparse.ArgumentParser()
+parser.add_argument('subreddit')
+parser.add_argument('tstamp1', type=crdate)
+parser.add_argument('tstamp2', type=crdate)
+args=parser.parse_args()
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'
+}
+
+dt1 = calendar.timegm(args.tstamp1)
+dt2 = calendar.timegm(args.tstamp2)
+url = "https://www.reddit.com/r/{}/search?q=timestamp%3A{}..{}&restrict_sr=on&sort=new&t=all&syntax=cloudsearch" .format(args.subreddit, dt1, dt2)
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, "html.parser")
+
+for link in soup.findAll(string=re.compile("i.imgur.com")):
+    print(link)
