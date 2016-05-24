36a560044c266127ce322c1a1b47a51d8ee75297
n0x5 <coax@audiolabs.org>
2016-05-24 20:22:34 +0200

    Update search-reddit.py

--- search-reddit.py
+++ search-reddit.py
@@ -2,15 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import time
-import sys
 import argparse
-from time import mktime
-from datetime import datetime
 import calendar
 import urllib.request
 
 def crdate(datestr):
-    return time.strptime(datestr, '%Y-%m-%d')
+    return calendar.timegm(time.strptime(datestr, '%Y-%m-%d'))
 
 parser=argparse.ArgumentParser()
 parser.add_argument('subreddit')
@@ -21,10 +18,7 @@ args=parser.parse_args()
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'
 }
-
-dt1 = calendar.timegm(args.tstamp1)
-dt2 = calendar.timegm(args.tstamp2)
-url = "https://www.reddit.com/r/{}/search?q=timestamp%3A{}..{}&restrict_sr=on&sort=new&t=all&limit=30&syntax=cloudsearch" .format(args.subreddit, dt1, dt2)
+url = "https://www.reddit.com/r/{}/search?q=timestamp%3A{}..{}&restrict_sr=on&sort=new&t=all&limit=10&syntax=cloudsearch" .format(args.subreddit, args.tstamp1, args.tstamp2)
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, "html.parser")
 
