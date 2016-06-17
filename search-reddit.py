2dfd1d49e06d718a31916f15260c9a458fb1b03e
n0x5 <coax@audiolabs.org>
2016-06-17 23:26:23 +0200

    Update search-reddit.py

--- search-reddit.py
+++ search-reddit.py
@@ -1,6 +1,7 @@
-#search-reddit.py - Search reddit for imgur.com pictures
+#!/usr/bin/env python
 
-#Example command - "python search-reddit.py pics 2015-06-05 2015-07-01" - where 'pics' is the subreddit name
+# search-reddit.py - Search reddit for imgur.com pictures
+# Example command - "python search-reddit.py pics 2015-06-05 2015-07-01" - where 'pics' is the subreddit name
 
 import requests
 from bs4 import BeautifulSoup
