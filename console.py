8be789f7bd71cca51b651fc4221982f68ebcd9c6
n0x5 <coax@audiolabs.org>
2016-06-17 23:17:21 +0200

    Update console.py

--- console.py
+++ console.py
@@ -20,11 +20,17 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'
 }
 
+b = open('console.html', 'a' )
+b.write('<!DOCTYPE html><html><body><h2>Games not released on PC</h2><table class="sortable" style="width:100%"><script src="sorttable.js"></script>')
+b.write('<tr><th style="text-align:left">Title</th><th style="text-align:left">Systems</th></tr>\n')
+
+
 url = 'http://www.gamespot.com/new-games/?sort=date&game_filter_type%5Bplatform%5D=94&game_filter_type%5BminRating%5D=&game_filter_type%5BtimeFrame%5D=&game_filter_type%5BstartDate%5D=&game_filter_type%5BendDate%5D=&game_filter_type%5Btheme%5D=&game_filter_type%5Bregion%5D=1&game_filter_type%5Bletter%5D=&page={}' .format(args.page) 
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, "html.parser")
 
 gameslist = soup.find('section', attrs={'class': 'editorial river'})
+
 
 def title(game):
     for title in row.find_all('div', class_="media-body") or row:
@@ -40,7 +46,7 @@ def systems(game):
 
 for row in gameslist.find_all('div', class_="media-body") or row:
     if systems(row) is not None:
-        b = open('console.txt', 'a' )
-        b.write('{}  {}\n' .format(title(row), systems(row)))
+        b.write("<tr><td class=\"title\">{}</td> <td class=\"systems\">{}</td></tr>\n" .format(title(row), systems(row)))
 
+b.write("</table>\n</body>\n</html>")
 b.close()
