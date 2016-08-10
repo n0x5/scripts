dc3bd7b5cd06f24df4bbafb0a6327a91d8d56a20
n0x5 <n0x5@users.noreply.github.com>
2016-08-10 21:24:23 +0200

    Update reimgur.py

--- reimgur.py
+++ reimgur.py
@@ -10,6 +10,7 @@
 #
 # can change the &limit=30 part up to 1000 but im not sure thats a good idea -
 # might get banned 
+# can also change the 'new' text in the url to 'hot', 'controversial' and so on
 
 import re
 import time
@@ -21,40 +22,15 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-def crdate(datestr):
-    return calendar.timegm(time.strptime(datestr, '%Y-%m-%d'))
-
-parser = argparse.ArgumentParser()
-parser.add_argument('subreddit')
-parser.add_argument('tstamp1', type=crdate)
-parser.add_argument('tstamp2', type=crdate)
-args = parser.parse_args()
-
-headers = {
-    'User-Agent': ('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36'
-                   ' (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36')
-}
-
-url = ('https://www.reddit.com/r/%s/search?q=timestamp%%3A%s..%s&restrict_sr=on&sort=new&t=all&limit=30&syntax=cloudsearch'
-       % (args.subreddit, args.tstamp1, args.tstamp2))
-
 class GrabIt(urllib.request.FancyURLopener):
-    version = ('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36'
-            ' (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36')
-    def download_file(self, url, path):
-            try:
-                urlretrieve = GrabIt().retrieve
-                urlretrieve(url, path)
-            except Exception as e:
-                print(str(e))
-
-
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, "html.parser")
-grab1 = GrabIt()
-
-
-print("searching", url)
+        version = ('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36'
+                ' (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36')
+        def download_file(self, url, path):
+                try:
+                    self.urlretrieve = GrabIt().retrieve
+                    self.urlretrieve(url, path)
+                except Exception as e:
+                    print(str(e))
 
 def get_single(soup, grab1):
     for link in soup.findAll(string=re.compile("i.imgur.com")):
@@ -75,11 +51,12 @@ def get_album(soup, headers, grab1):
         soup2 = BeautifulSoup(response2.text, "html.parser")
         for link2 in soup2.findAll('a', href=re.compile('\/\/i.imgur.com\/\w\w\w\w\w\w\w(.jpg)')):
             link3 = link2['href']
-            if os.path.isfile(link3[-11:]):
+            link4 = re.sub(r"[?]\d", "", link3)
+            if os.path.isfile(link4[-11:]):
                 print('file exists - skipping')
             else:
                 try:
-                    grab1.download_file('http:'+link3, link3[-11:])
+                    grab1.download_file('http:'+link3, link4[-11:])
                     print(link3.strip('/'))
                 except:
                     pass
@@ -95,6 +72,32 @@ def get_reddit_img(soup, grab1):
             except:
                 pass
 
-get_single(soup, grab1)
-get_album(soup, headers, grab1)
-get_reddit_img(soup, grab1)
+def main():
+    def crdate(datestr):
+        return calendar.timegm(time.strptime(datestr, '%Y-%m-%d'))
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('subreddit')
+    parser.add_argument('tstamp1', type=crdate)
+    parser.add_argument('tstamp2', type=crdate)
+    args = parser.parse_args()
+
+    headers = {
+        'User-Agent': ('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36'
+                       ' (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36')
+    }
+
+    url = ('https://www.reddit.com/r/%s/search?q=timestamp%%3A%s..%s&restrict_sr=on&sort=new&t=all&limit=30&syntax=cloudsearch'
+           % (args.subreddit, args.tstamp1, args.tstamp2))
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    grab1 = GrabIt()
+    get_single(soup, grab1)
+    get_album(soup, headers, grab1)
+    get_reddit_img(soup, grab1)
+    print("searching", url)
+
+if __name__ == "__main__":
+
+    main()
