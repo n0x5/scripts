cd581c3aecd126d5d2415f861c720f1cc3481d99
n0x5 <n0x5@users.noreply.github.com>
2016-11-13 21:17:18 +0100

    added more sections to ignore

--- wiki2html.py
+++ wiki2html.py
@@ -10,9 +10,9 @@
 # there will be bugs but it works ok on lostpedia
 # also excluded a bunch of non-content sections like Template and User talk
 
-
 import xml.etree.ElementTree as ET
 import os
+
 
 tree = ET.parse('your.xml')
 root = tree.getroot()
@@ -21,7 +21,8 @@ for elem in root.iter():
     if ('export-0.6/}title' in elem.tag and elem.text is not None and'User talk:' not in elem.text 
     and 'Template:' not in elem.text and 'Category:' not in elem.text and 'User:' not in elem.text
     and 'Shadowrun talk:' not in elem.text and 'File:' not in elem.text and 'Talk:' not in elem.text
-    and 'Shadowrun Wiki:' not in elem.text and 'Contributor:' not in elem.text):
+    and 'Shadowrun Wiki:' not in elem.text and 'Contributor:' not in elem.text
+    and 'User blog:' not in elem.text and 'User blog comment:' not in elem.text):
 
         fname = (os.path.join(elem.text.strip().replace('/', ' ').replace(':', '').replace('"', '')
         .replace('?', '').replace('!', '').replace('*', '').replace('\\', ' ')
@@ -33,7 +34,6 @@ for elem in root.iter():
             hfile.write('<h1>'.encode('utf8')+title.encode('utf8')+'</h1>'.encode('utf8'))
         except Exception as e:
             print(str(e))
-            
 
     if 'export-0.6/}text' in elem.tag and elem.text is not None:
         hfile.write('<link rel="stylesheet" href="style.css" type="text/css" media="screen" />'.encode('utf8'))
