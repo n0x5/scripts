d8d063bfd72813e645d18be7f19a2b9dd91c803e
n0x5 <n0x5@users.noreply.github.com>
2017-07-14 17:05:32 +0200

    scan for crap files in folders

---
+++ test_scan_crapmp3.py
@@ -0,0 +1,8 @@
+import os
+
+rootdir = r'/mp3/path'
+
+for subdir, dirs, files in os.walk(rootdir):
+    for fn in files:
+        if os.path.basename(subdir).lower()[4:10] not in fn.lower() and '.mp3' not in fn and .message not in fn and 'LABEL' not in fn:
+            print(subdir.lower()+'\\'+fn)
