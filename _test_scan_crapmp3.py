290729b8e2ce13b1073b49163ded61c5bf46bf43
n0x5 <n0x5@users.noreply.github.com>
2017-07-14 17:19:03 +0200

    new2

--- _test_scan_crapmp3.py
+++ _test_scan_crapmp3.py
@@ -1,8 +1,9 @@
 import os
 
-rootdir = r'/mp3/path'
+rootdir = r'/root/mp3'
 
 for subdir, dirs, files in os.walk(rootdir):
     for fn in files:
         if os.path.basename(subdir).lower()[4:10] not in fn.lower() and '.mp3' not in fn and '.message' not in fn and 'LABEL' not in fn:
             print(subdir.lower()+'\\'+fn)
+            #os.remove(os.path.join(subdir, fn)) # Uncomment to delete matching files
