dc1ab5c5e39fe8c936f9eab0d1c8d5ebf05fa289
n0x5 <n0x5@users.noreply.github.com>
2017-07-30 15:10:02 +0200

    updated

--- rsfv.py
+++ rsfv.py
@@ -7,22 +7,32 @@ import os
 import zlib
 
 rootdir = r'/path/to/folder'
-errors = 'errors.txt'
+
 
 for subdir, dirs, files in os.walk(rootdir):
     for fn in files:
         if fn.endswith('.sfv'):
             sfvf = open(os.path.join(subdir, fn), 'r')
-            sfver = open(os.path.join(rootdir, errors), 'w')
             for line in sfvf:
-                sfv1 = line.split()[1].lower()
-                sf1 = os.path.join(subdir, line.split()[0])
-                sf = open(sf1,'rb').read()
-                sf2 = format(zlib.crc32(sf), '08x')
-                if ';' in sf1:
-                    pass
-                if sfv1 != sf2:
-                    print('incorrect crc in {}' .format(sf1))
-                    sfver.write('incorrect crc in {}' .format(sf1))
+                try:
+                    sfv1 = line.split()[1].lower()
+                    sfv2 = line.split()[0].lower()
+                    sf1 = os.path.join(subdir, line.split()[0])
+                except Exception as e:
+                    print(str(e))
+                if not sfv2.startswith(';'):
+                    sf = open(sf1,'rb').read()
+                    sf2 = format(zlib.crc32(sf), '08x')
+                    if sfv1 != sf2:
+                        erlist.append(sf1)
+                        print('incorrect crc in {}' .format(sf1))
+                    else:
+                        print('crc ok {} {} {}' .format(sfv1, sf2, sf1))
                 else:
-                    print('crc ok {} {}' .format(sfv1, sf2))
+                    pass
+
+with open(os.path.join(rootdir, 'errors.txt'), 'w') as sfver:
+    for line in erlist:
+        print(line)
+        sfver.write(line+'\n')
+
