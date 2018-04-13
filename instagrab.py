fe3349a9cf3eb195bc9fd25ac73e0d4ef8f3ab71
n0x5 <n0x5@users.noreply.github.com>
2018-04-14 01:03:56 +0200

    yet another insta update, fixed

--- instagrab.py
+++ instagrab.py
@@ -5,16 +5,22 @@
 # python instagrab.py
 
 import re
-import requests
+from selenium import webdriver
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
+import sys
+import os
 import json
 import urllib.request
 from urllib.request import FancyURLopener
-import os
-import time
+import requests
 from tqdm import tqdm
+from bs4 import BeautifulSoup
 
 
 users = ['user1', 'user2', 'user3', 'user4', 'user5']
+
 
 class GrabIt(urllib.request.FancyURLopener):
         version = ('Mozilla/6.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36'
@@ -31,26 +37,39 @@ def grab_img(user):
     headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0'
         }
-    url = 'https://www.instagram.com/'+user+'/?__a=1'
-    response = requests.get(url, headers=headers)
-    data = json.loads(response.text)
-    files = data['graphql']['user']['edge_owner_to_timeline_media']['edges']
-
+    options = ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('disable-gpu')
+    driver = Chrome(chrome_options=options)
+    url = 'https://www.instagram.com/'+user+'/'
+    driver.get(url)
+    time.sleep(2)
+    html = driver.page_source
+    html = driver.page_source
+    soup = BeautifulSoup(html)
+    print(user)
+    table1 = soup.find('body')
+    table = table1.find('script', type=re.compile('text/javascript'))
+    json1 = table.get_text().replace('window._sharedData = ', '')[:-1]
+    data = json.loads(json1)
+    files = data['entry_data']['ProfilePage']
     for item in files:
-        full_url = item['node']['display_url']
-        filenm = os.path.basename(full_url)
-        endpoint1 = os.path.join(os.path.dirname(__file__), user, user+'_'+filenm)
-        time.sleep(1)
-        if not os.path.exists(user):
-                os.makedirs(user)
-        if os.path.isfile(endpoint1):
-            print('file {} exists - skipping' .format(endpoint1))
-        else:
-            try:
-                grab1.download_file(full_url, endpoint1)
-                print(full_url)
-            except Exception as e:
-                print(str(e))
+        full_url2 = item['graphql']['user']['edge_owner_to_timeline_media']['edges']
+        for item3 in full_url2:
+            full_url = item3['node']['display_url']
+            filenm = os.path.basename(full_url)
+            endpoint1 = os.path.join(os.path.dirname(__file__), user, user+'_'+filenm)
+            time.sleep(1)
+            if not os.path.exists(user):
+                    os.makedirs(user)
+            if os.path.isfile(endpoint1):
+                print('file {} exists - skipping' .format(endpoint1))
+            else:
+                try:
+                    grab1.download_file(full_url, endpoint1)
+                    print(full_url)
+                except Exception as e:
+                    print(str(e))
 
 for user in tqdm(users):
     print(user)
