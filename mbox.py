5ed81c207cb45ad5526757dea833c033d64b66b2
n0x5 <n0x5@users.noreply.github.com>
2021-03-28 18:57:14 +0200

    added time stamp parsing

--- mbox.py
+++ mbox.py
@@ -2,25 +2,27 @@
 
 import mailbox
 import sqlite3
-
+import time
+import datetime
 
 conn = sqlite3.connect('emails.db')
 cur = conn.cursor()
 
 cur.execute('''CREATE TABLE if not exists emails
-            (sender text, subject text, date_stamp text, message text, dated datetime DEFAULT CURRENT_TIMESTAMP)''')
+            (sender text, subject text, date_stamp text, u_stamp text, message text, dated datetime DEFAULT CURRENT_TIMESTAMP)''')
 
 for message in mailbox.mbox('All mail Including Spam and Trash.mbox'):
     try:
         subject = message['subject']
         sender = message['from']
         date = message['date']
+        u_stamp = datetime.datetime.strptime(message['date'], '%a, %d %b %Y %H:%M:%S %z')
         if message.is_multipart():
             content = ''.join(part.get_payload(decode=True) for part in message.get_payload())
         else:
             content = message.get_payload(decode=True)
-        print(sender, subject, date, content)
-        cur.execute('insert into emails (sender, subject, date_stamp, message) VALUES (?,?,?,?)', (sender, subject, date, content))
+        print(sender, subject, date, u_stamp, content)
+        cur.execute('insert into emails (sender, subject, date_stamp, u_stamp, message) VALUES (?,?,?,?,?)', (sender, subject, date, u_stamp, content))
         cur.connection.commit()
     except:
         pass
