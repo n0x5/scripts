88f4359c74de880826b3c2cd849f8d9da655b829
n0x5 <n0x5@users.noreply.github.com>
2021-07-13 17:45:05 +0200

    Add files via upload

---
+++ test_recursion_sqlite.py
@@ -0,0 +1,23 @@
+import sqlite3
+import os
+
+search = r'black'
+
+connection = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'movies44.db'))
+cursor = connection.cursor()
+
+
+def pages(lastvalue):
+    cursor.execute('select release, director from movies where release > ? order by release limit 8', (lastvalue,))
+    results = [(item[0], item[1]) for item in cursor.fetchall()]
+    lastvalue = results[7][0]
+    firstvalue = results[0][0]
+    print(results)
+    try:
+        pages(lastvalue)
+    except:
+        cursor.execute('select release, director from movies where release > ? order by release limit 8', (firstvalue,))
+        results = [(item[0], item[1]) for item in cursor.fetchall()]
+        print(results)
+
+pages('007.Die.Another.Day.PROPER.DVDRiP.XviD-DEiTY')
