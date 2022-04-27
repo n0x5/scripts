9125dd091425a10f2def30519dfa7df329b9d61d
n0x5 <n0x5@users.noreply.github.com>
2022-04-27 16:40:01 +0200

    skiplist

--- ia_downloader.py
+++ ia_downloader.py
@@ -35,33 +35,50 @@ get_items(s_url)
 
 print(video_list)
 
-for item3 in reversed(video_list):
-    m_url = 'https://archive.org/metadata/{}' .format(item3)
-    r2 = requests.get(m_url, headers=headers)
-    m_txt = json.loads(r2.text)
-    meta = m_txt['metadata']
-    title = meta['title'].replace(':', '').replace('?', '').replace('/', '').replace('"', '')
-    try:
-        date = meta['date']
-    except Exception:
-        pass
-    try:
-        final_name = title+'_'+date+'.mp4'
-    except Exception:
-        final_name = title+'.mp4'
-    download_url = 'https://archive.org/download/{}' .format(item3)
-    files = m_txt['files']
-    if not os.path.isfile(final_name):
-        try:
-            for item_file in files:
-                if 'h.264' in item_file['format']:
-                    download_url_final = download_url+'/'+item_file['name']
-                    r = requests.get(download_url_final, headers=headers)
-                    with open(final_name, 'wb') as fn:
-                        fn.write(r.content)
-                    print('downloaded', final_name)
-        except Exception as e:
-            print(final_name, 'failed', str(e))
-    else:
-        print('Skipping', final_name)
+with open('skiplist.txt', 'w') as fp:
+    pass
 
+for item3 in reversed(video_list):
+    f = open("skiplist.txt", "r")
+    test1 = f.read()
+    f.close()
+    if item3 not in test1:
+        m_url = 'https://archive.org/metadata/{}' .format(item3)
+        r2 = requests.get(m_url, headers=headers)
+        m_txt = json.loads(r2.text)
+        meta = m_txt['metadata']
+        title = meta['title'].replace(':', '').replace('?', '').replace('/', '').replace('"', '')
+        try:
+            date = meta['date']
+        except Exception:
+            pass
+        try:
+            final_name = title+'_'+date+'.mp4'
+        except Exception:
+            final_name = title+'.mp4'
+        download_url = 'https://archive.org/download/{}' .format(item3)
+        files = m_txt['files']
+        if not os.path.isfile(final_name):
+            try:
+                for item_file in files:
+                    if 'h.264' in item_file['format']:
+                        download_url_final = download_url+'/'+item_file['name']
+                        print('Downloading - ', download_url_final)
+                        r = requests.get(download_url_final, headers=headers)
+                        with open(final_name, 'wb') as fn:
+                            fn.write(r.content)
+                        print('downloaded', final_name)
+                        f = open("skiplist.txt", "a")
+                        f.write(item3+'\n')
+                        f.flush()
+                        f.close()
+            except Exception as e:
+                print(final_name, 'failed', str(e))
+        else:
+            f = open("skiplist.txt", "a")
+            f.write(item3+'\n')
+            f.flush()
+            f.close()
+            print('Skipping', final_name)
+    else:
+        print('Skipping, item3')
