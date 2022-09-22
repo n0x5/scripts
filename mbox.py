15a29a454c906a4c5171f2d46d86d96fe50cbbcb
n0x5 <n0x5@users.noreply.github.com>
2022-09-22 16:08:23 +0200

    timestamp fix

--- mbox.py
+++ mbox.py
@@ -8,14 +8,15 @@ import time
 import datetime
 import tqdm
 
-conn = sqlite3.connect('alt_cyberpunk.db')
+
+conn = sqlite3.connect('alt_tv_buffy.db')
 cur = conn.cursor()
 
 cur.execute('''CREATE TABLE if not exists emails
             (sender text, subject text, date_stamp text, u_stamp text, message text, dated datetime DEFAULT CURRENT_TIMESTAMP)''')
 
 lst = []
-for message in tqdm.tqdm(mailbox.mbox('alt.cyberpunk.mbox', factory=BytesParser(policy=default).parse)):
+for message in tqdm.tqdm(mailbox.mbox('alt.tv.buffy-v-slayer.mbox', factory=BytesParser(policy=default).parse)):
     try:
         subject = message['subject']
         sender = message['from']
@@ -23,7 +24,10 @@ for message in tqdm.tqdm(mailbox.mbox('alt.cyberpunk.mbox', factory=BytesParser(
         try:
             u_stamp = datetime.datetime.strptime(message['date'], '%a, %d %b %Y %H:%M:%S %z')
         except:
-            u_stamp = 'Null'
+            try:
+                u_stamp = datetime.datetime.strptime(message['date'], '%Y/%m/%d')
+            except:
+                u_stamp = 'Null'
         if message.is_multipart():
             try:
                 content = ''.join(part.get_payload(decode=False) for part in message.get_payload())
@@ -36,7 +40,7 @@ for message in tqdm.tqdm(mailbox.mbox('alt.cyberpunk.mbox', factory=BytesParser(
                 content = ''
         stuff = str(sender), str(subject), str(date), str(u_stamp), content
         lst.append(stuff)
-        if len(lst) == 500:
+        if len(lst) == 2000:
             cur.executemany('insert into emails (sender, subject, date_stamp, u_stamp, message) VALUES (?,?,?,?,?)', (lst))
             cur.connection.commit()
             lst = []
