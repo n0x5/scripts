666b4975a090adaa09ed0a17772fb0e60fe556cc
n0x5 <n0x5@users.noreply.github.com>
2022-10-07 18:28:56 +0200

    fix

--- Blogger.py
+++ Blogger.py
@@ -1,3 +1,4 @@
+from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
