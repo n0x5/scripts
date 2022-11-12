5bc8a873b4e1eaa188b3bdd7dc60e1360de44cee
n0x5 <n0x5@users.noreply.github.com>
2022-11-12 12:27:55 +0100

    download the entire internet in a day ez with this

---
+++ webarchive.py
@@ -0,0 +1,74 @@
+# Download a whole website from internet web archive
+# Example command:
+# python webarchive.py en.wikipedia.org --download
+#
+# Show only number of results: 
+# python webarchive.py en.wikipedia.org --pages
+#
+
+
+import requests
+import json
+import os
+import argparse
+import time
+import html
+
+parser = argparse.ArgumentParser()
+parser.add_argument('url')
+parser.add_argument('--pages', action='store_const', const=1)
+parser.add_argument('--download', action='store_const', const=1)
+args = parser.parse_args()
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/49.0'
+    }
+
+
+url_pages = r'http://web.archive.org/cdx/search/cdx?url={}&matchType=prefix&output=json&filter=statuscode:200&collapse=urlkey&fl=timestamp,mimetype,original,urlkey&limit=1000&page=0&showNumPages=true' .format(args.url)
+
+
+
+def show_pages():
+    r = requests.get(url_pages, headers=headers)
+    print('Number of pages: {}, which is about {} results' .format(r.text.strip(), int(r.text.strip()) * 1000))
+
+def download_pages():
+    rcount = requests.get(url_pages, headers=headers)
+    dl_folder = args.url
+    if not os.path.exists(dl_folder):
+        os.makedirs(dl_folder)
+    for item in range(int(rcount.text.strip())):
+        print('Downloading page {} of {} total pages' .format(item, rcount.text.strip()))
+        url = r'http://web.archive.org/cdx/search/cdx?url={}&matchType=prefix&output=json&filter=statuscode:200&collapse=urlkey&fl=timestamp,mimetype,original,urlkey&limit=1000&page={}' .format(args.url, item)
+        r = requests.get(url, headers=headers)
+        data = json.loads(r.text)
+
+        for item_url in data:
+            if 'urlkey' not in item_url:
+                dl_url = 'https://web.archive.org/web/'+item_url[0]+'if_/'+item_url[2]
+                with open('{}_urls.txt' .format(args.url), 'a', encoding='utf-8') as fn2:
+                    fn2.write(dl_url+'\n')
+                url_file = item_url[3].split(')/')
+
+                if url_file is not None:
+                    endpoint = os.path.join(dl_folder, url_file[1].replace('"', '').replace('\\', '').replace('\\\\', ''))
+                    print(dl_url)
+                    if not os.path.exists(endpoint):
+                        try:
+                            r_dl = requests.get(dl_url, headers=headers)
+                            with open(endpoint, 'w', encoding='utf-8') as fn:
+                                fn.write(r_dl.text)
+                                time.sleep(1)
+                        except Exception as e:
+                            print('skipping', dl_url)
+
+if args.pages == 1:
+    show_pages()
+
+if args.download == 1 and 'http' not in args.url:
+    download_pages()
+else:
+    print("Don't put http:// or https:// in url. https://en.wikipedia.org should be en.wikipedia.org")
+
