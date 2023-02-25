d35ba07e80c53bf5208ae5a0f36bc59f5b3f71dd
n0x5 <n0x5@users.noreply.github.com>
2023-02-25 11:27:56 +0100

    file name fix

--- fandom_api_to_sqlite.py
+++ fandom_api_to_sqlite.py
@@ -119,6 +119,8 @@ def dl_images(url_img):
         name = item['name']
         title = item['title']
         url = item['url']
+        timestamp = item['timestamp']
+        name = re.sub(r'[\;*?!<>|/:"]', '', str(name))
         print(name)
         endpoint = os.path.join('{}_images' .format(wiki_name), name)
         if not os.path.exists(endpoint):
@@ -128,8 +130,8 @@ def dl_images(url_img):
                     cover_jpg.write(r.content)
             except Exception as e:
                 print(e)
-            stuff = name, title
-            cur.execute('insert or ignore into {}_images (name, title) VALUES (?,?)' .format(wiki_name), (stuff))
+            stuff = name, title, timestamp
+            cur.execute('insert or ignore into {}_images (name, title, timestamp) VALUES (?,?,?)' .format(wiki_name), (stuff))
             cur.connection.commit()
             time.sleep(delay)
     if 'continue' in data:
@@ -139,7 +141,7 @@ def dl_images(url_img):
 
 if args.download_images == 1:
     cur.execute('''CREATE TABLE if not exists {}_images
-        (name text, title text)''' .format(wiki_name))
+        (name text, title text, timestamp text)''' .format(wiki_name))
     os.makedirs('{}_images' .format(wiki_name), exist_ok=True)
     url_img = 'https://{}.fandom.com/api.php?action=query&list=allimages&format=json&export=wikitext&ailimit=500' .format(wiki_name)
     dl_images(url_img)
