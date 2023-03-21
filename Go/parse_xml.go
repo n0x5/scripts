019c37a57dbab10b6976a200e2090b5e65c7b4e9
n0x5 <n0x5@users.noreply.github.com>
2023-03-21 14:05:05 +0100

    Create parse_xml.go

---
+++ Go/parse_xml.go
@@ -0,0 +1,60 @@
+// Unfinished script to parse big xml file
+// Learning Go so this is just a test
+// https://discogs-data-dumps.s3.us-west-2.amazonaws.com/index.html?prefix=data/2023/
+
+package main
+
+import (
+    "encoding/xml"
+    "fmt"
+    "os"
+    "strings"
+)
+
+type Release struct {
+    XMLName xml.Name `xml:"release"`
+    Style     []string    `xml:"styles>style"`
+    Genre     []string    `xml:"genres>genre"`
+    Label     []Label    `xml:"labels>label"`
+    Track     []string    `xml:"tracklist>track>title"`
+    RelTitle    string   `xml:"title"`
+    MasterID    int   `xml:"master_id"`
+    Country    string   `xml:"country"`
+    Released    string   `xml:"released"`
+    Artist      string   `xml:"artists>artist>name"`
+}
+
+type Label struct {
+    Name    string  `xml:"name,attr"`
+    Catno    string  `xml:"catno,attr"`
+    ID    int  `xml:"id,attr"`
+}
+
+
+func main() {
+    xmlFile, err := os.Open("discogs_20230301_releases.xml")
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    fmt.Println("Successfully Opened releases.xml")
+    defer xmlFile.Close()
+
+    d := xml.NewDecoder(xmlFile)
+    for {
+        t, tokenErr := d.Token()
+        if tokenErr != nil {
+            fmt.Println(err)
+        }
+        switch t := t.(type) {
+        case xml.StartElement:
+            b := &Release{}
+            if err := d.DecodeElement(&b, &t); err != nil {
+                fmt.Println(err)
+            }
+            fmt.Printf("%s - %d - %s - %s - %s - %v - %v - %v\n", b.Released, b.MasterID, b.RelTitle, b.Country, b.Artist, strings.Join(b.Genre, ", "), strings.Join(b.Style, ", "), b.Label)
+        }
+    }
+}
+
+
