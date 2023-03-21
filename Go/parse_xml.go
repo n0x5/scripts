326cc3f75079d97f5376d7871e246834f2e07df3
n0x5 <n0x5@users.noreply.github.com>
2023-03-21 16:51:55 +0100

    works but unsure how to improve it

--- Go/parse_xml.go
+++ Go/parse_xml.go
@@ -1,4 +1,5 @@
-// Unfinished script to parse big xml file
+// Unfinished script to parse big xml file into sqlite3
+// On windows need: https://jmeubank.github.io/tdm-gcc/
 // Learning Go so this is just a test
 // https://discogs-data-dumps.s3.us-west-2.amazonaws.com/index.html?prefix=data/2023/
 
@@ -9,7 +10,11 @@ import (
     "fmt"
     "os"
     "strings"
+    "database/sql"
+    "log"
+    _ "github.com/mattn/go-sqlite3"
 )
+
 
 type Release struct {
     XMLName xml.Name `xml:"release"`
@@ -19,7 +24,7 @@ type Release struct {
     Track     []Track    `xml:"tracklist>track"`
     RelTitle    string   `xml:"title"`
     MasterID    int   `xml:"master_id"`
-    ID    int   `xml:"id"`
+    ID    int   `xml:"id,attr"`
     Country    string   `xml:"country"`
     Released    string   `xml:"released"`
     Artist      string   `xml:"artists>artist>name"`
@@ -38,6 +43,20 @@ type Track struct {
 }
 
 func main() {
+    db, err := sql.Open("sqlite3", "test555.db")
+    var i int
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    sql1 := `create table if not exists discogs (released text, master_id int, id int, rel_title text, country text, 
+            artist text, genres text, styles text, label_id int, label_name text, label_catno text, tracklist text);`
+    _, err = db.Exec(sql1)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
     xmlFile, err := os.Open("discogs_20230301_releases.xml")
     if err != nil {
         fmt.Println(err)
@@ -45,7 +64,6 @@ func main() {
 
     fmt.Println("Successfully Opened releases.xml")
     defer xmlFile.Close()
-
     d := xml.NewDecoder(xmlFile)
     for {
         t, tokenErr := d.Token()
@@ -58,10 +76,13 @@ func main() {
             if err := d.DecodeElement(&b, &t); err != nil {
                 fmt.Println(err)
             }
-            fmt.Printf("%s - %d - %d - %s - %s - %s - %v - %v - %d - %s - %s - %v\n", b.Released, b.MasterID, b.ID, b.RelTitle, b.Country, b.Artist, 
-                        strings.Join(b.Genre, ", "), strings.Join(b.Style, ", "), b.Label.ID, b.Label.Name, b.Label.Catno, b.Track)
+            sql2 := fmt.Sprintf(`insert into discogs (released, master_id, id, rel_title, country, artist, genres, styles, label_id, label_name, label_catno, tracklist) values ('%s','%d','%d','%s','%s','%s','%v','%v','%d','%s','%s','%v');\n`, b.Released, b.MasterID, b.ID, b.RelTitle, b.Country, b.Artist, 
+                       strings.Join(b.Genre, ", "), strings.Join(b.Style, ", "), b.Label.ID, b.Label.Name, b.Label.Catno, b.Track)
+            _, err = db.Exec(sql2)
+            i++;
+            fmt.Println(i)
         }
     }
+    defer db.Close()
 }
-
 
