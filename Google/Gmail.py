40105eb80d91f302084da83cd68bbc9b3abf6b8d
n0x5 <n0x5@users.noreply.github.com>
2023-08-10 10:17:48 +0200

    some small fixes

--- Google/Gmail.py
+++ Google/Gmail.py
@@ -41,45 +41,52 @@ def parse(nextpage=None):
         service = build('gmail', 'v1', credentials=creds)
         token = service._http.credentials.token
         headers = {'Authorization': 'Bearer {}' .format(token)}
-        payload = {'pageToken': nextpage}
         url4 = 'https://gmail.googleapis.com/gmail/v1/users/{}/messages/' .format(userid)
         if nextpage:
+            payload = {'pageToken': nextpage, 'maxResults': 500}
             res = requests.get(url4, headers=headers, params=payload)
         else:
-            res = requests.get(url4, headers=headers)
+            payload = {'maxResults': 500}
+            res = requests.get(url4, headers=headers, params=payload)
         data = json.loads(res.text)
         for item in tqdm(data['messages']):
             try:
-                msg = 'https://gmail.googleapis.com/gmail/v1/users/{}/messages/{}' .format(userid, item['id'])
-                res2 = requests.get(msg, headers=headers)
-                data2 = json.loads(res2.content)
-                data3 = res2.text
-                threadid = data2['threadId']
-                id_msg = data2['id']
-                labelids = str(data2['labelIds'])
-                snippet = data2['snippet']
-                internaldate = data2['internalDate']
+                cur.execute('select exists(select 1 from gmail where id_msg = ? LIMIT 1)', (item['id'],))
+                record = cur.fetchone()
+                if record[0] == 1:
+                    print('Message already saved', item['id'])
+                else:
+                    msg = 'https://gmail.googleapis.com/gmail/v1/users/{}/messages/{}' .format(userid, item['id'])
+                    res2 = requests.get(msg, headers=headers)
+                    data2 = json.loads(res2.content)
+                    data3 = res2.text
+                    threadid = data2['threadId']
+                    id_msg = data2['id']
+                    labelids = str(data2['labelIds'])
+                    snippet = data2['snippet']
+                    internaldate = data2['internalDate']
 
-                try:
-                    body = base64.urlsafe_b64decode(data2['payload']['parts'][1]['body']['data'])
-                except Exception:
-                    for item2 in data2['payload']['parts']:
-                        body += base64.urlsafe_b64decode(item2['body']['data'])
+                    try:
+                        body = base64.urlsafe_b64decode(data2['payload']['parts'][1]['body']['data'])
+                    except Exception:
+                        for item2 in data2['payload']['parts']:
+                            body += base64.urlsafe_b64decode(item2['body']['data'])
 
-                for item3 in data2['payload']['headers']:
-                    if 'To' in item3['name']:
-                        to = item3['value']
-                    if 'From' in item3['name']:
-                        from_sender = item3['value']
-                    if 'Subject' in item3['name']:
-                        subject = item3['value']
-                    if 'Date' in item3['name']:
-                        date = item3['value']
-                stuff = id_msg, labelids, threadid, snippet, to, from_sender, subject, date, body, int(internaldate), str(data3)
-                stuff2 = id_msg, labelids, threadid, snippet, to, from_sender, subject, date, int(internaldate)
-                cur.execute('insert or ignore into gmail (id_msg, labelids, threadid, snippet, recipient, from_sender, subject, date_sent, body, internaldate, json_data) VALUES (?,?,?,?,?,?,?,?,?,?,?)', (stuff))
-                cur.connection.commit()
-                print(stuff2)
+                    for item3 in data2['payload']['headers']:
+                        if 'To' in item3['name']:
+                            to = item3['value']
+                        if 'From' in item3['name']:
+                            from_sender = item3['value']
+                        if 'Subject' in item3['name']:
+                            subject = item3['value']
+                        if 'Date' in item3['name']:
+                            date = item3['value']
+                    stuff = id_msg, labelids, threadid, snippet, to, from_sender, subject, date, body, int(internaldate), str(data3)
+                    stuff2 = id_msg, labelids, threadid, snippet, to, from_sender, subject, date, int(internaldate)
+                    cur.execute('insert or ignore into gmail (id_msg, labelids, threadid, snippet, recipient, from_sender, subject, date_sent, body, internaldate, json_data) VALUES (?,?,?,?,?,?,?,?,?,?,?)', (stuff))
+                    cur.connection.commit()
+                    print(stuff2)
+
             except Exception as e:
                 print(e)
                 pass
