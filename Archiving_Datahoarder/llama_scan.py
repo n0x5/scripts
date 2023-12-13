281c73c51af1c6b23e481152c58937c845d840cb
n0x5 <n0x5@users.noreply.github.com>
2023-12-13 23:35:38 +0100

    Update llama_scan.py

--- Archiving_Datahoarder/llama_scan.py
+++ Archiving_Datahoarder/llama_scan.py
@@ -11,6 +11,7 @@
 # https://developer.nvidia.com/cuda-downloads
 #
 # add "cl.exe" to PATH (C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.38.33130\bin\Hostx64\x64\) on mine
+# guide: https://www.youtube.com/watch?v=d1Fnfvat6nM
 
 
 import subprocess
